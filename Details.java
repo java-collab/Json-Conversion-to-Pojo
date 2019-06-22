906465ff21463860a58e2b9524aa4a53640d8ab0
shanigarapu srinivas <50327738+seenu1095@users.noreply.github.com>
2019-06-22 16:29:25 +0530

    Defining Pojo Class To Deserialize json object

---
+++ Details.java
@@ -0,0 +1,45 @@
+package com.demo;
+
+/**
+ * @author srinivas.shanigarapu
+ *
+ *         Defining pojo class for details.
+ */
+public class Details {
+
+	private String name;
+
+	private String age;
+
+	private String city;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Details [name=" + name + ", age=" + age + ", city=" + city + "]";
+	}
+
+}
